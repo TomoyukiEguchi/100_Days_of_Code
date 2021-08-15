62ecef821599c5990862fc2e66c4e111584a6e52
TomoyukiEguchi <tomoyukieguchi1004@gmail.com>
2021-08-15 19:10:50 +0900

    initial commit

---
+++ snake_game/snake.py
@@ -0,0 +1,28 @@
+from turtle import Turtle
+
+
+STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
+MOVE_DISTANCE = 20
+
+
+class Snake:
+
+    def __init__(self):
+        self.segments = []
+        self.create_snake()
+
+    def create_snake(self):
+        for position in STARTING_POSITIONS:
+            body = Turtle(shape="square")
+            body.color("white")
+            body.penup()
+            body.goto(position)
+            self.segments.append(body)
+
+    def move(self):
+        for seg_num in range(len(self.segments) - 1, 0, -1):
+            new_x = self.segments[seg_num - 1].xcor()
+            new_y = self.segments[seg_num - 1].ycor()
+            self.segments[seg_num].goto(new_x, new_y)
+
+        self.segments[0].forward(MOVE_DISTANCE)

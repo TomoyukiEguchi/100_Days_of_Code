3fa79ce6977cb2aba29ee67dd192871f0622f9f7
TomoyukiEguchi <tomoyukieguchi1004@gmail.com>
2021-08-23 18:01:04 +0900

    pandas basic

--- day-25/main.py
+++ day-25/main.py
@@ -16,5 +16,34 @@
 
 import pandas
 
-data = pandas.read_csv("weather_data.csv")
-print(data["temp"])
+# data = pandas.read_csv("weather_data.csv")
+# print(data)
+#
+# data_dict = data.to_dict()
+# print(data_dict)
+#
+# temp_list = data["temp"].to_list()
+# print(temp_list)
+#
+# print(data["temp"].mean())
+# print(data["temp"].max())
+
+# Get Data in Row
+# print(data[data.day == "Monday"])
+# print(data[data.temp == data.temp.max()])
+
+# monday_row = data[data.day == "Monday"]
+# celsius = int(monday_row.temp)
+# print((celsius * 1.8) + 32)
+
+# Create a dataframe from scratch
+data_dict = {
+    "students": ["Amy", "James", "Angela"],
+    "scores": [76, 56, 65]
+}
+
+data = pandas.DataFrame(data_dict)
+# print(data)
+data.to_csv("new_data.csv")
+
+
